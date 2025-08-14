042d29031d68d5f1e9b7505d6a7534435fb9d5a7
kworrix <kworrix@me.com>
2025-08-14 12:06:20 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,35 @@
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <header style={{
+        display: 'flex',
+        alignItems: 'center',
+        padding: '12px 24px',
+        borderBottom: '1px solid #eee',
+        fontFamily: 'system-ui, sans-serif'
+      }}>
+        {/* Logo links back to home */}
+        <a href="/" style={{ display: 'flex', alignItems: 'center', marginRight: 'auto' }}>
+          <img
+            src="/logo.jpeg"
+            alt="PraiseMix Logo"
+            style={{ height: 40, marginRight: 12 }}
+          />
+          <span style={{ fontSize: 20, fontWeight: 600 }}>PraiseMix</span>
+        </a>
+
+        {/* Navigation links */}
+        <nav>
+          <a href="/" style={{ marginRight: 16 }}>Home</a>
+          <a href="/about" style={{ marginRight: 16 }}>About</a>
+          <a href="/contact" className="underline">Contact</a>
+        </nav>
+      </header>
+
+      {/* Page content */}
+      <Component {...pageProps} />
+    </>
+  );
 }
