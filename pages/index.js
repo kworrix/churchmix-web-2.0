284e1ecad6ecf8239b766d03505f5d4feb15f07a
kworrix <kworrix@me.com>
2025-08-14 01:43:59 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ export default function Home() {
     <main style={{ padding: 24, fontFamily: 'system-ui, sans-serif' }}>
       <h1>Hello from ChurchMix</h1>
       <p>If you can see this page, the build worked 🎉</p>
+
+      {/* The link must be INSIDE the component JSX */}
+      <a className="underline" href="/about">About</a>
     </main>
   );
 }
-<a className="underline text-blue-600" href="/about">About</a>
