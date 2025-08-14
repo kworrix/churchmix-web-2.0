5bad97360c32f29eac30758782d47b41cbe8acfa
kworrix <kworrix@me.com>
2025-08-14 01:50:02 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24, fontFamily: 'system-ui, sans-serif' }}>
+      <h1>Hello from ChurchMix</h1>
+      <p>If you can see this page, the build worked 🎉</p>
+      <a className="underline" href="/about">About</a>
+    </main>
+  );
+}
