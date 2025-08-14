ef64e505ecb4418ed025b54a5dadedca3fb5bfaf
kworrix <kworrix@me.com>
2025-08-14 01:07:56 -0400

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{padding:24}}>
-      <h1>Hello from ChurchMix</h1>
-      <p>If you can see this page, the build worked 🎉</p>
-    </main>
-  );
-}
