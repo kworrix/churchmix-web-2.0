d81925431d0cef5e6365365bc2226abbfafbb735
kworrix <kworrix@me.com>
2025-08-14 01:39:40 -0400

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <main style={{ padding: 24, fontFamily: 'system-ui, sans-serif' }}>
-      <h1>About ChurchMix</h1>
+      <h1>About ChurchMix 🚀</h1>
       <p>ChurchMix helps churches find vetted broadcast mix engineers and pay safely.</p>
     </main>
   );
