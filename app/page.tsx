68e17feb8185a2989e0a0416ff70882e2701ed9b
kworrix <kworrix@me.com>
2025-08-14 00:59:10 -0400

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding:24}}>
+      <h1>Hello from ChurchMix</h1>
+      <p>If you can see this page, the build worked 🎉</p>
+    </main>
+  );
+}
