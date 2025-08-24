043353208226fd8c09e8f4823ad7c760a0ba936c
kworrix <kworrix@me.com>
2025-08-23 20:05:43 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,10 +10,13 @@ export default function App({ Component, pageProps }) {
             <span className="font-semibold text-lg text-gray-900">PraiseMix</span>
           </a>
           <nav className="ml-auto flex items-center gap-5">
-            <a href="/" className="no-underline">Home</a>
-            <a href="/about" className="no-underline">About</a>
-            <a href="/contact" className="no-underline">Contact</a>
-          </nav>
+  <a href="/" className="no-underline">Home</a>
+  <a href="/about" className="no-underline">About</a>
+  <a href="/contact" className="no-underline">Contact</a>
+  <a href="/signup/church" className="no-underline font-semibold">Church Sign Up</a>
+  <a href="/signup/engineer" className="no-underline">Engineer Sign Up</a>
+</nav>
+
         </div>
       </header>
       <Component {...pageProps} />
